c166e42a7b517f901d20d7cd7ef7e02a287cb361
Sander Ettema <Github@dobbe8.nl>
2025-12-06 21:04:08 +0100

    Comment-out two import statements

--- betting/admin.py
+++ betting/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group, User
-from django.db.models import Count, Q
+# from django.db.models import Count, Q
 from django.utils import timezone
 
 from .models import Bet, BetType, Competition, CompetitionStanding, Driver, Race, RaceResult, UserProfile
